3d394c440f2946ca84f840a406a01b735f056e9f
Davidtt083 <92117303+Davidtt083@users.noreply.github.com>
2024-03-06 14:08:08 -0600

    puerto

--- main.py
+++ main.py
@@ -45,4 +45,4 @@ def home():
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+    app.run(debug=True, port=int(os.environ.get('PORT', 4000)))
